937ac07ab2b21ef4e5351d3a01351e3212132cc1
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-08-12 14:05:01 -0600

    Add files via upload

---
+++ HolaMundoCOLOR.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    std::cout << "Historia de la computacion:\n\n" << std::endl;
+
+    std::cout << "Primera Generación (1940-1950):\n"
+              << "Durante esta época, las computadoras eran enormes máquinas que funcionaban con válvulas electrónicas. Ejemplos notables incluyen la ENIAC y la UNIVAC. Eran muy costosas, generaban mucho calor y requerían un mantenimiento constante..\n\n";
+
+    std::cout << "Segunda Generación (1950-1960):\n"
+              << "Esta etapa vio la introducción de transistores en lugar de válvulas, lo que resultó en computadoras más pequeñas, eficientes y confiables. Las máquinas de esta generación usaban lenguajes de programación más avanzados y comenzaron a utilizarse para aplicaciones científicas y comerciales.\n\n";
+
+    std::cout << "Tercera Generación (1960-1970):\n"
+              << "Surgieron los circuitos integrados, que permitieron más componentes en un solo chip. Esto llevó a computadoras más rápidas y a la reducción de costos. Los sistemas operativos también se desarrollaron en esta época, lo que facilitó la interacción entre usuarios y computadoras.\n\n";
+
+    std::cout << "Cuarta Generación (1970-1980):\n"
+              << "Se produjo la miniaturización de los circuitos integrados, lo que dio lugar a las microcomputadoras o PCs. Surgieron las primeras computadoras personales, como la Apple II y la IBM PC. También se desarrollaron las interfaces gráficas y los sistemas operativos más amigables.\n\n";
+
+    std::cout << "Quinta Generación (1980-Presente):\n"
+              << "Se caracteriza por avances en la capacidad de procesamiento paralelo, inteligencia artificial y tecnologías de redes. Surgieron las supercomputadoras y las estaciones de trabajo de alto rendimiento. Los dispositivos móviles, la nube y la interconectividad global son características clave de esta generación.\n\n";                 
+
+    return 0;
+}
