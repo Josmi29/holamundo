963b6d54e4e6d8c4261df1dd0d1385d147210399
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-08-12 14:24:12 -0600

    Update HolaMundoCOLOR.cpp

--- HolaMundoCOLOR.cpp
+++ HolaMundoCOLOR.cpp
@@ -7,20 +7,22 @@ int main() {
     SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     std::cout << "Historia de la computacion:\n\n" << std::endl;
 
-    std::cout << "Primera Generación (1940-1950):\n"
-              << "Durante esta época, las computadoras eran enormes máquinas que funcionaban con válvulas electrónicas. Ejemplos notables incluyen la ENIAC y la UNIVAC. Eran muy costosas, generaban mucho calor y requerían un mantenimiento constante..\n\n";
+    std::cout << "Primera GeneraciÃ³n (1940-1950):\n"
+              << "Durante esta Ã©poca, las computadoras eran enormes mÃ¡quinas que funcionaban con vÃ¡lvulas electrÃ³nicas. Ejemplos notables incluyen la ENIAC y la UNIVAC. Eran muy costosas, generaban mucho calor y requerÃ­an un mantenimiento constante..\n\n";
 
-    std::cout << "Segunda Generación (1950-1960):\n"
-              << "Esta etapa vio la introducción de transistores en lugar de válvulas, lo que resultó en computadoras más pequeñas, eficientes y confiables. Las máquinas de esta generación usaban lenguajes de programación más avanzados y comenzaron a utilizarse para aplicaciones científicas y comerciales.\n\n";
+    std::cout << "Segunda GeneraciÃ³n (1950-1960):\n"
+              << "Esta etapa vio la introducciÃ³n de transistores en lugar de vÃ¡lvulas, lo que resultÃ³ en computadoras mÃ¡s pequeÃ±as, eficientes y confiables. Las mÃ¡quinas de esta generaciÃ³n usaban lenguajes de programaciÃ³n mÃ¡s avanzados y comenzaron a utilizarse para aplicaciones cientÃ­ficas y comerciales.\n\n";
 
-    std::cout << "Tercera Generación (1960-1970):\n"
-              << "Surgieron los circuitos integrados, que permitieron más componentes en un solo chip. Esto llevó a computadoras más rápidas y a la reducción de costos. Los sistemas operativos también se desarrollaron en esta época, lo que facilitó la interacción entre usuarios y computadoras.\n\n";
+    std::cout << "Tercera GeneraciÃ³n (1960-1970):\n"
+              << "Surgieron los circuitos integrados, que permitieron mÃ¡s componentes en un solo chip. Esto llevÃ³ a computadoras mÃ¡s rÃ¡pidas y a la reducciÃ³n de costos. Los sistemas operativos tambiÃ©n se desarrollaron en esta Ã©poca, lo que facilitÃ³ la interacciÃ³n entre usuarios y computadoras.\n\n";
 
-    std::cout << "Cuarta Generación (1970-1980):\n"
-              << "Se produjo la miniaturización de los circuitos integrados, lo que dio lugar a las microcomputadoras o PCs. Surgieron las primeras computadoras personales, como la Apple II y la IBM PC. También se desarrollaron las interfaces gráficas y los sistemas operativos más amigables.\n\n";
+    std::cout << "Cuarta GeneraciÃ³n (1970-1980):\n"
+              << "Se produjo la miniaturizaciÃ³n de los circuitos integrados, lo que dio lugar a las microcomputadoras o PCs. Surgieron las primeras computadoras personales, como la Apple II y la IBM PC. TambiÃ©n se desarrollaron las interfaces grÃ¡ficas y los sistemas operativos mÃ¡s amigables.\n\n";
 
-    std::cout << "Quinta Generación (1980-Presente):\n"
-              << "Se caracteriza por avances en la capacidad de procesamiento paralelo, inteligencia artificial y tecnologías de redes. Surgieron las supercomputadoras y las estaciones de trabajo de alto rendimiento. Los dispositivos móviles, la nube y la interconectividad global son características clave de esta generación.\n\n";                 
-
+    std::cout << "Quinta GeneraciÃ³n (1980-Presente):\n"
+              << "Se caracteriza por avances en la capacidad de procesamiento paralelo, inteligencia artificial y tecnologÃ­as de redes. Surgieron las supercomputadoras y las estaciones de trabajo de alto rendimiento. Los dispositivos mÃ³viles, la nube y la interconectividad global son caracterÃ­sticas clave de esta generaciÃ³n.\n\n";                 
+    std::cout << "RESUMEN:\n"
+              << "Las computadoras han evolucionado a lo largo de dÃ©cadas. En sus inicios, eran mÃ¡quinas enormes con vÃ¡lvulas electrÃ³nicas. Luego, llegaron los transistores, que hicieron que las computadoras fueran mÃ¡s pequeÃ±as y eficientes. DespuÃ©s vinieron los circuitos integrados, lo que las hizo aÃºn mÃ¡s rÃ¡pidas y accesibles. los chips se hicieron mas pequeÃ±os y esto llevÃ³ al surgimiento de las computadoras personales en la dÃ©cada de 1970. La conectividad global y la inteligencia artificial caracterizan la quinta generaciÃ³n. Hoy en dÃ­a, la tecnologÃ­a sigue avanzando con la computaciÃ³n cuÃ¡ntica y la inteligencia artificial avanzada. Cada etapa ha revolucionado cÃ³mo interactuamos con la informaciÃ³n y ha transformado numerosos aspectos de nuestras vidas.\n";
+	
     return 0;
 }
